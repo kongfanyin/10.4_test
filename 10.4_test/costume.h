65d2fc1749ae78a17a649a0d063dc0f4f6f3317c
kongfanyin <kong.engineer@foxmail.com>
2022-12-23 18:43:21 +0800

    添加项目文件。

---
+++ 10.4_test/costume.h
@@ -0,0 +1,8 @@
+#pragma once
+struct customer
+{
+	char FullName[30];
+	double payment;
+
+
+};
---
+++ 10.4_test/main.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<string>
+#include<fstream>
+#include<iostream>
+
+int main(char argc, char** argv)
+{
+
+
+
+
+	return 0;
+}
