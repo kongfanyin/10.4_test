80eaa5863630ca69eff82badc146fd0597a47f2e
kongfanyin <kong.engineer@foxmail.com>
2022-12-23 21:22:30 +0800

    10.4

--- 10.4_test/costume.h
+++ 10.4_test/costume.h
@@ -1,8 +1,35 @@
 #pragma once
-struct customer
+#ifndef Stack_
+#define Stack_
+#include<iostream>
+#include<string>
+typedef customer Item;
+struct  customer
 {
 	char FullName[30];
 	double payment;
-
-
 };
+class Stack
+{
+public:
+	Stack() { strcpy(c1.FullName, '\0'), c1.payment = 0.0, n = 0; };
+	~Stack();
+	void push_cs();
+	void pop_cs();
+private:
+	Item c1;
+	int n;
+};
+
+Stack::Stack()
+{
+
+}
+
+Stack::~Stack()
+{
+
+
+
+}
+#endif // !Stack_
--- 10.4_test/main.cpp
+++ 10.4_test/main.cpp
@@ -1,4 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+//编写一个程序，它从栈中添加和customer结构（栈用Stack类声明表示）。每次customer 结构被删除时，其payment的值都被加入到总数中，并报告总数
+//注意，应该可以使用Stack类而不做修改；只需修改typedef声明，使Item的类型为customer，而不是unsigned long 即可。
 
 #include<string>
 #include<fstream>
